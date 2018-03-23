b03b7c472e27d0f946b5c95feac656eb15bf511b
Colin Woodbury <colingw@gmail.com>
2018-03-23 09:03:12 -0700

    Use `streaming-attoparsec`

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@
 module Main ( main ) where
 
 import           Control.Monad ((>=>))
+import           Control.Monad.Trans.Resource
 import           Data.Aeson (Value, json)
 import qualified Data.Attoparsec.ByteString.Streaming as A
 import qualified Data.ByteString.Streaming as Q
