99547b6549be5fa7a6e6de65831e3067fb296c98
Colin Woodbury <colingw@gmail.com>
2018-03-23 14:11:16 -0700

    Avoid horrible space leak
    - And return more information

--- src/Main.hs
+++ src/Main.hs
@@ -7,13 +7,14 @@ import           Control.Monad.Trans.Resource
 import           Data.Aeson (Value, json)
 import qualified Data.Attoparsec.ByteString.Streaming as A
 import qualified Data.ByteString.Streaming as Q
-import           Data.Kanji
+import           Data.Kanji hiding (densities)
 import           Data.Maybe (mapMaybe)
+import           Data.Semigroup
 import qualified Data.Text as T
-import           Lens.Micro ((^?))
+import           Lens.Micro
 import           Lens.Micro.Aeson (key, _String)
 import qualified Shelly as Sh
-import           Streaming
+import           Streaming hiding (Sum, (<>))
 import qualified Streaming.Prelude as S
 
 ---
@@ -40,11 +41,27 @@ jsons = void . A.parsed json . Q.readFile
 texts :: FilePath -> Stream (Of T.Text) RIO ()
 texts fp = S.mapMaybe (^? key "text" . _String) $ S.for (files fp) jsons
 
-elementary :: Stream (Of T.Text) RIO () -> Stream (Of Float) RIO ()
-elementary = S.map (elementaryDen . levelDist . mapMaybe kanji . T.unpack)
+data Densities = Densities !Float !Float !Float deriving (Show)
+
+instance Semigroup Densities where
+  Densities a b c <> Densities a' b' c' = Densities (a + a') (b + b') (c + c')
+
+instance Monoid Densities where
+  mempty  = Densities 0 0 0
+  mappend = (<>)
+
+densities :: T.Text -> Densities
+densities t = Densities e (m - e) (h - m)
+  where ld = levelDist . mapMaybe kanji $ T.unpack t
+        e  = elementaryDen ld
+        m  = middleDen ld
+        h  = highDen ld
 
 main :: IO ()
 main = do
   putStrLn "Starting..."
-  runResourceT (S.sum_ . elementary $ texts "data/out/") >>= print . (/ 1097409)
+  Densities e m h <- runResourceT . S.mconcat_ . S.map densities $ texts "data/out/"
+  putStrLn $ "Elementary: "    <> show (100 * e / 1097409)
+  putStrLn $ "Middle School: " <> show (100 * m / 1097409)
+  putStrLn $ "High School: "   <> show (100 * h / 1097409)
   putStrLn "Complete."
